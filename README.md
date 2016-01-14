1411a2e873a87f80bdeb7ef783826396b9118393
Julppu <juha.lindqvist@cs.helsinki.fi>
2016-01-14 14:27:15 +0200

    first commit, readme etc

---
+++ README.md
@@ -0,0 +1,9 @@
+#Dotfiles
+Repository for my personal dotfiles for several \*nix programs. Contains a simple script to copy and move all files to home directory.  
+
+Just run
+''' bash
+git clone git@github.com:Julppu/dotfiles.git
+cd dotfiles
+./deploy.sh
+'''
---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+########################################
+#                                      #
+# Deploy dotfiles to Linux home        #
+#                                      #
+# Copies contents of this repository   #
+# to your home directory with a single #
+# script. Fast and easy                #
+#                                      #
+########################################
