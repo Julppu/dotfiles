48f9e1332257143aee51b5afc84762a697c6a4ca
Julppu <lindqvist.juha@gmail.com>
2019-03-28 08:06:00 +0200

    moved homebrew env var definitions

---
+++ zsh/.zsh/homebrew_paths.zsh
@@ -0,0 +1,59 @@
+# Homebrew binaries to path - set precedence to homebrew installed
+# reverse order: last set is first searched when executing
+export PATH="/usr/local/opt/icu4c/bin:$PATH"
+export PATH="/usr/local/opt/icu4c/sbin:$PATH"
+export PATH="/usr/local/opt/libpcap/bin:$PATH"
+export PATH="/usr/local/opt/guile@2.0/bin:$PATH"
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="/usr/local/lib/ruby/gems/2.6.0/bin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/ed/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/curl/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/openssl:$PATH"
+export PATH="/usr/local/sbin:$PATH"
+
+# compiler flag paths
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/openssl/lib"
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/ruby/lib"
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/curl/lib"
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/sqlite/lib"
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/guile@2.0/lib"
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/readline/lib"
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/libpcap/lib"
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/icu4c/include"
+export LDFLAGS="$LDFLAGS:-L/usr/local/opt/binutils/include"
+
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/openssl/include"
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/ruby/include"
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/curl/include"
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/sqlite/include"
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/guile@2.0/include"
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/readline/include"
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/libpcap/include"
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/icu4c/include"
+export CPPFLAGS="$CPPFLAGS:-I/usr/local/opt/binutils/include"
+
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/openssl/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/ruby/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/curl/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/sqlite/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/guile@2.0/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/readline/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/libpcap/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/icu4c/lib/pkgconfig"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/binutils/lib/pkgconfig"
+
+export RUBYLIB="$RUBYLIB:/usr/local/lib/ruby"
+export GROOVY_HOME="/usr/local/opt/groovy/libexec"
+
+# update PATH for the Google Cloud SDK.
+if [ -f '/Users/jlindqvist/Downloads/google-cloud-sdk/path.zsh.inc' ];
+    then . '/Users/jlindqvist/Downloads/google-cloud-sdk/path.zsh.inc';
+fi
+
+# enable shell command completion for gcloud.
+if [ -f '/Users/jlindqvist/Downloads/google-cloud-sdk/completion.zsh.inc' ];
+    then . '/Users/jlindqvist/Downloads/google-cloud-sdk/completion.zsh.inc';
+fi
